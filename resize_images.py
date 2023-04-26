22335e527a504621fe928bc474a5f5531b734884
Sabrina Tochkov <sabrina.tochkov@gmail.com>
2023-04-26 13:11:20 -0400

    Add Lab 1 - Resizing Images with PIL library

---
+++ resize_images.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import os, glob
+from PIL import Image
+
+for file in glob.glob("ic_*"):
+    im = Image.open(file).convert('RGB)
+    im.rotate(270).resize((128,128)).save("/opt/icons/" + file, "JPEG")
